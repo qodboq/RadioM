a609c6d13b9048fe0e97872ffe56dad47543ab0d
Erik Valigurský <erik.vali@icloud.com>
2024-08-06 10:01:31 +0200

    Initial Commit

---
+++ RadioM/RadioMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioMApp.swift
+//  RadioM
+//
+//  Created by Erik Valigurský on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RadioMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
